0277968ac890282b7e54bde147dd9bfc9d993bf0
Frank Groeneveld <frank@webindie.nl>
2024-05-06 17:18:11 +0200

    Support on premise Active Directory

--- lib/omniauth/strategies/azure_activedirectory_v2.rb
+++ lib/omniauth/strategies/azure_activedirectory_v2.rb
@@ -52,12 +52,13 @@ module OmniAuth
         options.custom_policy =
           provider.respond_to?(:custom_policy) ? provider.custom_policy : nil
 
-        options.client_options.authorize_url = "#{options.base_azure_url}/#{options.tenant_id}/oauth2/v2.0/authorize"
+        oauth2 = provider.respond_to?(:adfs?) && provider.adfs? ? 'oauth2' : 'oauth2/v2.0'
+        options.client_options.authorize_url = "#{options.base_azure_url}/#{options.tenant_id}/#{oauth2}/authorize"
         options.client_options.token_url =
           if options.custom_policy
-            "#{options.base_azure_url}/#{options.tenant_id}/#{options.custom_policy}/oauth2/v2.0/token"
+            "#{options.base_azure_url}/#{options.tenant_id}/#{options.custom_policy}/#{oauth2}/token"
           else
-            "#{options.base_azure_url}/#{options.tenant_id}/oauth2/v2.0/token"
+            "#{options.base_azure_url}/#{options.tenant_id}/#{oauth2}/token"
           end
 
         super
